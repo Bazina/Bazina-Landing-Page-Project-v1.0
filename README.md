ebeb76cb18b60534ff482daf11df0451fdace2e0
Youssef Bazina <60438793+Bazina@users.noreply.github.com>
2020-12-12 23:26:28 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# Bazina-Landing-Page-Project-v1.0
+# Landing Page Project
+
+## Table of Contents
+
+- [Instructions](#instructions)
+- [Run](#Run)
+- [ES6](#es6)
+- [Changes](#changes)
+- [New](#new)
+
+## Instructions
+
+The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. That will require modifying the HTML and CSS files, but primarily the JavaScript file.
+
+To get started, open `js/app.js` and start building out the app's functionality
+
+For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+
+## Run
+
+To open this project locally, download the project then open `index.html`
+
+## ES6 & Jquery
+
+I used many ES6 methods & some Jquery, i used
+- `arrow functions`
+- `const & let` declaration
+- `IntersectionObserver`
+
+These thing are working with the latest browsers except IE 11, IE 10.
+
+## Changes
+Almost all changes have happened in the `js/app.js` file & some few changes in `index.html` & very few changes in the styling `css/styles.css`
+
+## New
+
+- Built A dynamical navigation bar based on the number of sections that be hidden while not scrolling.
+- Designed an active state for section and its navigation bar element.
+- When clicking an item from the navigation menu, the link should scroll to the appropriate section.
+- A collapsible sections are made.
+- Designed a scroll to top button on the page that’s only visible when the user scrolls below the fold of the page.
---
+++ js/app.js
@@ -0,0 +1,149 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/* Define Global Variables */
+
+const navBar = document.getElementById("navbar__list");
+const secElements = document.querySelectorAll("section");
+
+/* End Global Variables */
+
+/* build the nav, Build menu, this snippet was built with the help of GitHub */
+
+secElements.forEach(sec => {
+    const navBarElement = `<li class="menu__link" data-link="${sec.id}" ><a href = "#${sec.id}">${sec.dataset.nav}</a></li> `;
+    navBar.insertAdjacentHTML("beforeend",navBarElement);
+});
+
+/* Scroll to anchor ID using scrollTO event,
+* Scroll to section on link click
+*Using window.scrollTo
+*/
+
+navBar.addEventListener("click", evt =>{
+    evt.preventDefault();
+    const ancestor = evt.target.hasAttribute("data-link") ? evt.target : evt.target.parentElement;
+    const elScroll = document.getElementById(ancestor.dataset.link);
+    let firstX = window.scrollX;
+    let firstY = window.scrollY;
+    let nowX = elScroll.getBoundingClientRect().x;
+    let nowY = elScroll.getBoundingClientRect().y;
+    let goX = nowX + firstX;
+    let goY = nowY + firstY;
+    window.scrollTo({
+        left : goX,
+        top : goY,
+        behavior : "smooth"
+    });
+});
+
+/* Add class 'active' to section when near top of viewport,
+    Set sections as active 
+* Using Intersection Observer
+* entries is a callback function
+*/
+
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        const navBarElement = document.querySelector(
+        `.menu__link[data-link='${entry.target.id}']`
+        );
+        const secActive = document.getElementById(entry.target.id);
+        if (entry.isIntersecting) {
+        navBarElement.classList.add('active')
+        secActive.classList.add('active')
+        } else {
+        if (navBarElement.classList.contains('active')) {
+            navBarElement.classList.remove('active')
+        }
+        if (secActive.classList.contains('active')) {
+            secActive.classList.remove('active')
+        }
+        }
+    })
+},
+    {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.7,
+    }
+    )
+secElements.forEach(el => {
+    observer.observe(document.getElementById(el.id))
+});
+
+/* Hide navBar while scrolling, src: jquery,
+ was sended by my tutor at a discussion */
+
+$(window).scroll(function() {
+    $("#navbar__menu").slideDown(500);
+    clearTimeout($.data(this, 'scrollTimer'));
+    $.data(this, 'scrollTimer', setTimeout(function() {
+        // after user scroll for the first time start the function if user didn't
+     //do something if user not scrolled
+    $("#navbar__menu").slideUp(500);
+    }, 1000));
+    
+});
+
+/* Add toTop Button */
+
+const newBtn = document.createElement("button");
+newBtn.innerHTML = "Top";
+document.body.appendChild(newBtn);
+newBtn.setAttribute("id", "toTop")
+
+/* Scroll to Top */
+
+/* When the user scrolls down 20px from the top of the document,
+    show the button, this snippet built with the help of
+  "https://www.w3schools.com/howto/howto_js_scroll_to_top.asp" */
+
+window.onscroll = () =>{
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    newBtn.style.display = "block";
+    } else {
+    newBtn.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+
+newBtn.onclick = () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    })
+}
+
+/* Animated Collapsible Sections,
+    this snippet built with the help of
+    "https://www.w3schools.com/howto/howto_js_collapsible.asp" */
+
+const coll = document.getElementsByClassName("collapsible");
+
+for (let i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("activecoll");
+        let content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = `${content.scrollHeight}px`;
+    }
+    });
+}
